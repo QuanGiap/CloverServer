944f260a09392a3df6c8fd52c31b82b4d0937e7a
Quan Giap <minhquangiapnguyen@gmail.com>
2025-03-29 23:37:42 -0700

    Update User.js

--- functions/models/User.js
+++ functions/models/User.js
@@ -1,87 +1,35 @@
-// import verifyToken from '@/tool/verifyToken';
-// import { NextResponse } from 'next/server';
-
-// const fake_data_play = [
-// {
-//     id:123,
-//     placeName: "Space Needle",
-//     flagImgUrl:'ebe9e823cc09db35643e5d89fb6476bd.png',
-//     address: "400 Broad St, Seattle, WA 98109, United States",
-//     date: new Date("1995-12-17T03:24:00"),
-//     point: 728,
-//     task: 10,
-//     maxTask: 10,
-//     time: "07:48",
-// },
-// {
-//     id:456,
-//     placeName: "Space Needle",
-//     flagImgUrl:'ebe9e823cc09db35643e5d89fb6476bd.png',
-//     date: new Date("2000-01-01T10:00:00"),
-//     address: "400 Broad St, Seattle, WA 98109, United States",
-//     point: 500,
-//     task: 5,
-//     maxTask: 10,
-//     time: "10:30",
-// },
-// {
-//     id:132,
-//     placeName: "Space Needle",
-//     flagImgUrl:'ebe9e823cc09db35643e5d89fb6476bd.png',
-//     address: "400 Broad St, Seattle, WA 98109, United States",
-//     date: new Date("2010-05-15T14:30:00"),
-//     point: 850,
-//     task: 8,
-//     maxTask: 10,
-//     time: "14:45",
-// }
-// ];
-// const fake_stamp_data = [
-// {
-//     id:123,
-//     imgUrl: "space needle.svg",
-//     stamped: true,
-// },
-// {
-//     id:213,
-//     imgUrl: "second tower.png",
-//     stamped: false,
-// },
-// {
-//     id:312,
-//     imgUrl: "eiffel.svg",
-//     stamped: false,
-// },
-// {
-//     id:456,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// {
-//     id:654,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// {
-//     id:546,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// {
-//     id:564,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// {
-//     id:789,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// {
-//     id:978,
-//     imgUrl: "clover.png",
-//     stamped: false,
-// },
-// ];
+const { auth, db } = require("../FireBase/FireBase");
 // async function getPlayHistory(req, user_id) {}
-// module.exports = { getPlayHistory };
+
+/**
+ * Return user data, return null if not exist
+ * @param {string} email
+ * @return {object|null} userData
+ */
+async function getUserByEmail(email) {
+  try {
+    const user = await auth.getUserByEmail(email);
+    const userData = await db.collection("user").doc(user.uid).get();
+    return userData;
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+}
+
+/**
+ * Create new user
+ * @param {string} userEmail
+ * @return {Promise} userData
+ */
+async function createUser(userEmail) {
+  const user = await auth.createUser({
+    email: userEmail,
+  });
+  const userData = await db.collection("user").doc(user.uid).set({
+    stamps: [],
+    email: userEmail,
+  });
+  return userData;
+}
+module.exports = { getUserByEmail, createUser };
